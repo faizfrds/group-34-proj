e9c248eb0cb358ddcf72135c46682dc9da0c49cb
faiz <faiz.firdaus.2004@gmail.com>
2024-04-02 15:08:48 -0400

    title and vision

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -4,28 +4,28 @@ const teamNames = [
     id: "faiz",
     grade: "Sophomore",
     major: "Computer Science",
-    bio: "I have some experience with web development projects. Can do some TypeScript, JavaScript, Java, Python, C. Experience and knowledge of postgreSQL, Git, Prisma, REST API.  I`m mostly familiar with building using frameworks, not too familiar with vanilla javascript/html/css. Can do some design/frontend as well as backend if needed.",
+    bio: "Faiz is experienced in doing frontend and backend development as well using databases. He can do design work using Figma and also develop frontend using Tailwind CSS. He has some experience with PostgreSQL.",
   },
   {
     name: "Ryan Kirchner",
     id: "ryan",
     grade: "Junior",
     major: "Informatics (DS)",
-    bio: "I am proficient in JavaScript, HTML, CSS,Typescript,  HTML/CSS. I have experience developing web applications. My other skills less relevant to the project include Python, Java, R, Linux, MS Office, MS Visual Studio, MS SQL Server Management Studio. I am willing to fill any role whether it’s design, programming or a mixture of both",
+    bio: "Ryan is proficient in JavaScript, HTML, CSS,Typescript, and has experience developing web applications. He also has skills in other tech-related tools such as MS Office, MS Visual Studio, MS SQL Server Management Studio.",
   },
   {
     name: "Luke Walsh",
     id: "luke",
     grade: "Junior",
     major: "Computer Science",
-    bio: "I have plentiful experience with Python, java, javascript, and typeScript, as well as a foundational understanding of C++ and HTML. I also have a background in Data Science, as I was a data science major for a year and a half at Bryant University until I transferred here. I have always been comfortable with working in teams and can work on any task I`m assigned and always have my work done on time.",
+    bio: "Luke has plentiful experience with Python, java, javascript, and typeScript, as well as a foundational understanding of C++ and HTML. He also possesses a background in Data Science, as he was a data science major",
   },
   {
     name: "Sumrudhi Jadhav",
     id: "sumrudhi",
     grade: "Sophomore",
     major: "Computer Science",
-    bio: "In regards to web development, I have worked with TypeScript, JavaScript, HTML, and CSS. However, I am not too well versed in HTML and CSS. I also have experience with Ruby on Rails, Git, Java, Python, C/C++, SQL, MERN Stack, and Swift.",
+    bio: "In regards to web development, Sumrudhi has worked with TypeScript, JavaScript, HTML, and CSS. She also has experience with Ruby on Rails, Git, Java, Python, C/C++, SQL, MERN Stack, and Swift.",
   },
 ];
 
@@ -36,33 +36,33 @@ const addNames = () => {
     nameElement.className = "pt-5 mx-4 text-justify w-full";
     // nameElement.textContent = member.name;
 
-    let namePic = document.createElement("div");
+    let namePic = document.createElement("div"); //name and pic container
 
     let memberName = document.createElement("p");
     memberName.className = "text-lg font-semibold";
     memberName.textContent = member.name;
 
-    // let picContainer = document.createElement("div");
-    // picContainer.className = "h-10 w-10 rounded-full bg-black";
-    // let pic = document.createElement("img");
-    // pic.src = `https://cdn.pixabay.com/photo/2018/07/08/14/16/cat-3523992_640.jpg`;
-    // pic.className = "h-2 w-2 rounded-full object-fit"
-    // picContainer.appendChild(pic);
+    let picContainer = document.createElement("div");
+    picContainer.className = "h-10 w-10 rounded-full bg-black";
+    let pic = document.createElement("img");
+    pic.src = "public/ryan.jpg";
+    
+    pic.className = "h-2 w-2 rounded-full object-fit"
+    picContainer.appendChild(pic);
 
     let gradeMajor = document.createElement("p");
     gradeMajor.className = "text-md font-normal";
     gradeMajor.textContent = member.grade + " • " + member.major;
 
-    // namePic.appendChild(pic);
+    namePic.appendChild(pic);
     namePic.appendChild(memberName);
     namePic.appendChild(gradeMajor);
 
-    let bio = document.createElement("p");
+    let bio = document.createElement("p"); //bio written on separate container
     bio.className = "text-sm font-normal pt-5 text-justify";
     bio.textContent = member.bio;
 
     nameElement.appendChild(namePic);
-    // nameElement.appendChild(gradeMajor);
     nameElement.appendChild(bio);
     teamSection.appendChild(nameElement);
   });
