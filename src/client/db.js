8e36dffbb1edfb1b4463c68dbad23af05eac88b2
faiz <faiz.firdaus.2004@gmail.com>
2024-04-24 23:21:22 -0400

    added jsdocs for db.js

--- src/client/db.js
+++ src/client/db.js
@@ -1,39 +1,63 @@
 class PouchDBManager {
-    constructor(dbName) {
-      this.db = new PouchDB(dbName);
-    }
-  
-    async saveData(key, data) {
-      try {
-        await this.db.put({
-          _id: key,
-          data: data
-        });
-        console.log(`Data saved for key: ${key}`);
-      } catch (error) {
-        console.error(`Error saving data for key ${key}:`, error);
-      }
-    }
-  
-    async getData(key) {
-      try {
-        const result = await this.db.get(key);
-        console.log(`Data retrieved for key: ${key}`);
-        return result.data;
-      } catch (error) {
-        console.error(`Error retrieving data for key ${key}:`, error);
-        return null;
-      }
-    }
-  
-    async deleteData(key) {
-      try {
-        await this.db.remove(key);
-        console.log(`Data deleted for key: ${key}`);
-      } catch (error) {
-        console.error(`Error deleting data for key ${key}:`, error);
-      }
+  /**
+   * Creates an instance of PouchDB.
+   *
+   * @param {string} dbName The name of the PouchDB database.
+   */
+  constructor(dbName) {
+    this.db = new PouchDB(dbName);
+  }
+
+  /**
+   * Saving data to database asynchronously, send error when unsuccessful
+   *
+   * @param {string} key - id for the data
+   * @param {string} data - Data to be saved
+   * @returns {void}
+   */
+  async saveData(key, data) {
+    try {
+      await this.db.put({
+        _id: key,
+        data: data,
+      });
+      console.log(`Data saved for key: ${key}`);
+    } catch (error) {
+      console.error(`Error saving data for key ${key}:`, error);
     }
   }
-  
-  export default PouchDBManager;
+
+  /**
+   * Fetching data from database asynchronously, send error when unsuccessful
+   *
+   * @param {string} key - id for the data
+   * @returns {void}
+   */
+  async getData(key) {
+    try {
+      const result = await this.db.get(key);
+      console.log(`Data retrieved for key: ${key}`);
+      return result.data;
+    } catch (error) {
+      console.error(`Error retrieving data for key ${key}:`, error);
+      return null;
+    }
+  }
+
+  /**
+   * Deleting data from database asynchronously, send error when unsuccessful
+   *
+   * @param {string} key - id for the data
+   * @returns {void}
+   */
+  async deleteData(key) {
+    try {
+      await this.db.remove(key);
+      console.log(`Data deleted for key: ${key}`);
+    } catch (error) {
+      console.error(`Error deleting data for key ${key}:`, error);
+    }
+  }
+}
+
+export default PouchDBManager;
